e7f122389b542b04da13ce9b68d3e11641af60ee
Brain8torm <brainstorm@programist.ru>
2023-02-28 15:32:42 +0300

    config

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 const config = {
-
+    baseUrl: 'https://cats.petiteweb.dev/api/single/Brain8torm',
+    headers: {
+        'content-type': 'application/json',
+    },
 };
 
-class Api {
-
-}
+class Api {}
 
 export const api = new Api(config);
